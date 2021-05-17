039757ae7d5f863a8babe702073962579de46f15
clx <hbdennischen@gmail.com>
2021-05-17 11:23:10 +0800

    dev init

--- startup.sh
+++ startup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-export RUNNING_MODE=prod
+export RUNNING_MODE=dev
 export LOGLEVEL=debug
 export SERIAL_PORT=ON
 export PORT=9510
-export MONGODB=mongodb://192.168.5.166:27017/Cross
-export MQ=amqp://jsm:jsm@192.168.5.166
+export mongodb://localhost:27017/Livingforest
+export amqp://qladapfm:CjtgA21O-1Ux-L108UCR70TcJ4GDpRVh@spider.rmq.cloudamqp.com/qladapfm
 export JWT_SECRET=DBCEBERVEQVB1945G4GRG
-node server.js
+node --trace-warnings server.js
