68921d1f0d65f45b9d49ee6cf3bdc7bd7ebf6a0b
clx <hbdennischen@gmail.com>
2021-05-31 14:56:36 +0800

    set to https

--- server.js
+++ server.js
@@ -1,14 +1,23 @@
-const createServer = require('@finelets/hyper-rest/express/CreateServer'),
-	messageCenterConfig = require('./server/MessageCenterConfig'),
-	jwtConfig = require('./server/JwtConfig')(),
-	flow = require('./server/Flow')
+const fs = require("fs"),
+	createServer = require("@finelets/hyper-rest/express/CreateServer"),
+	messageCenterConfig = require("./server/MessageCenterConfig"),
+	jwtConfig = require("./server/JwtConfig")(),
+	flow = require("./server/Flow");
 
-const config = {
-	appName: 'livingforest',
+let config = {
+	appName: "livingforest",
 	baseDir: __dirname,
 	messageCenterConfig,
 	jwtConfig,
-	flow
+	flow,
+};
+
+if (process.env.HTTPS) {
+	const cert = {
+		key: fs.readFileSync("./.cert/server.key"),
+		cert: fs.readFileSync("./.cert/server.crt")
+	}
+	config = {...config, cert}
 }
 
-createServer(config)
+createServer(config);
