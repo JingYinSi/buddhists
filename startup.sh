8fb8ea31f42fb9058b1fc2b8a2c4eb3ab8d0d67a
clx1608@qq.com <hbdennischen@gmail.com>
2021-06-18 16:32:18 +0800

    docker-compose ok

--- startup.sh
+++ startup.sh
@@ -2,8 +2,9 @@
 export RUNNING_MODE=rest
 export LOGLEVEL=debug
 export PORT=9550
-#export MONGODB=mongodb://localhost:27017/Livingforest
-export MONGODB=mongodb://localhost:28515/Livingforest
-export MQ=amqp://qladapfm:CjtgA21O-1Ux-L108UCR70TcJ4GDpRVh@spider.rmq.cloudamqp.com/qladapfm
+export MONGODB=mongodb://localhost:27017/Livingforest
+#export MONGODB=mongodb://localhost:28515/Livingforest
+#export MQ=amqp://qladapfm:CjtgA21O-1Ux-L108UCR70TcJ4GDpRVh@spider.rmq.cloudamqp.com/qladapfm
+export MQ=amqp://jsm:jsm@localhost:5672
 export JWT_SECRET=DBCEBERVEQVB1945G4GRG
 node --trace-warnings server.js
