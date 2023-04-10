d2f1d4b6bc45b2ee8c3c4a26e4d6b9004342cac2
19041287 <19041287@suning.com>
2023-04-10 17:21:54 +0800

    完善日历功能

--- server/biz/mygdh/Calendar.js
+++ server/biz/mygdh/Calendar.js
@@ -1,6 +1,7 @@
 const schema = require('../../../db/schema/mygdh/Calendar'),
     createEntity = require('@finelets/hyper-rest/db/mongoDb/DbEntity'),
     {extend} = require('underscore')
+
 const config = {
     schema,
     projection: ['gongLiDay', 'zangLiDay', 'specialDay'],
@@ -11,6 +12,18 @@ const config = {
 
 const entity = createEntity(config)
 const obj = {
+    findByDay: function (day) {
+        let condi = {"gongLiDay": day}
+        let text
+        return entity.search(condi, text)
+            .then(function (list) {
+                if (list.length > 0) {
+                    return list[0];
+                } else {
+                    return undefined;
+                }
+            })
+    }
 }
 
 module.exports = extend(entity, obj)
--- server/rests/Calendar.js
+++ server/rests/Calendar.js
@@ -1,34 +1,24 @@
 /**
  *  课程
  */
-const {
-    ifMatch,
-    update,
-    remove,
-    findById
-} = require('../biz/mygdh/Calendar')
-moment = require('moment')
-const logger = require('@finelets/hyper-rest/app/Logger')
-const entity = require("../biz/mygdh/Calendar");
-const list = function (req,res) {
-    let day =moment().format("yyyyMMDD")
-    let condi = {"gongLiDay": day}
-    let text
-    return entity.search(condi, text)
-        .then(function (list) {
-            if(list.length>0){
-                return res.status(200).json(list[0]);
-            } else {
-                return res.status(500);
-            }
-        })
-};
-
+const entity = require("../biz/mygdh/Calendar"),
+    moment = require('moment'),
+    logger = require('@finelets/hyper-rest/app/Logger');
 module.exports = {
     url: '/api/calendar/current',
     rests: [{
         type: 'http',
-        method:'get',
-        handler: list
+        method: 'get',
+        handler: function (req, res) {
+            let day = moment().format("yyyy-MM-DD")
+            return entity.findByDay(day).then(function (data) {
+                if (!data) {
+                    logger.error("未找到指定的日期(" + day + ")的日历数据")
+                    return res.status(500).end();
+                } else {
+                    return res.status(200).json(data);
+                }
+            })
+        }
     }]
 }
