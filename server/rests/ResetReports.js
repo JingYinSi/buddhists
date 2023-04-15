1198ceb891c0b33e4a417c42d1cfec8a8ec19408
ssice2631 <ssice2631@163.com>
2023-04-15 16:00:56 +0800

    dev-0415

--- server/rests/ResetReports.js
+++ server/rests/ResetReports.js
@@ -12,11 +12,10 @@ module.exports = {
         type: 'http',
         method: 'put',
         handler: (req, res) => {
-
-            lessonEntity.resetLessonIns(req.body)
-
-            wxUserEntity.resetUserLesson(req.body)
-
+            if (req.body.flag === 'day') {
+                lessonEntity.resetLessonIns(req.body)
+                wxUserEntity.resetUserLesson(req.body)
+            }
             return wxUserEntity.resetUserLessonIns(req.body)
                 .then(ok => {
                     return res.status(200).end()
